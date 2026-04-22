f09c395019732fb187093e77671359b6922e22de
Asmit Kandwal <asmitkandwal17@gmail.com>
2026-04-22 09:42:35 +0530

    Add solution: 3541. Find Most Frequent Vowel and Consonant

---
+++ 3541. Find Most Frequent Vowel and Consonant/3541. Find Most Frequent Vowel and Consonant.cpp
@@ -0,0 +1,40 @@
+/*
+ * Problem: 3541. Find Most Frequent Vowel and Consonant
+ * Difficulty: Easy
+ * Link: https://leetcode.com/problems/find-most-frequent-vowel-and-consonant/submissions/1984985394/
+ * Language: cpp
+ * Date: 2026-04-22
+ */
+
+class Solution {
+public:
+    int maxFreqSum(string s) {
+
+        unordered_map<char, int> mpp;
+        int vowel = 0;
+        int consonent = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            // Update frequency of current character
+            mpp[s[i]]++;
+
+            int temp = mpp[s[i]];   // current character frequency
+
+            // Track highest frequency among vowels
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' ||
+                s[i] == 'o' || s[i] == 'u') {
+
+                vowel = max(temp, vowel);
+            }
+
+            // Track highest frequency among consonants
+            else {
+                consonent = max(temp, consonent);
+            }
+        }
+
+        // Required answer = max vowel freq + max consonant freq
+        return vowel + consonent;
+    }
+};
